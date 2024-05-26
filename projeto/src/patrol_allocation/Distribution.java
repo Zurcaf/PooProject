8edbbd4a3431b65af7983e73640d5f5ba762b559
FranciscoPRosa <114608777+FranciscoPRosa@users.noreply.github.com>
2024-05-26 01:19:34 +0100

    Distribution.java alterado, distribuição inicial de patrulhas feita

--- projeto/src/patrol_allocation/Distribution.java
+++ projeto/src/patrol_allocation/Distribution.java
@@ -1,13 +1,31 @@
 package patrol_allocation;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Distribution {
-
+	Random r = new Random();
 	private final int[][] array;
 
-	Distribution(int[][] array) {
-		this.array = array;
+	public Distribution(int patrols, int systems) {
+        int[] allocS = new int [systems]; 
+        int[] allocP = new int [patrols];
+        for (int i = 0; i<systems; i++){
+            int systemP = r.nextInt(0,patrols);
+            allocS[i] = systemP;
+            allocP[systemP]++;
+        }
+        int[][] array = new int[patrols][];
+        for (int i = 0; i < patrols; i++) {
+            array[i] = new int[allocP[i]];
+        }  
+        int[] patrolIndexes = new int[patrols];  
+        for (int i = 0; i < systems; i++) {
+            int patrol = allocS[i];
+            array[patrol][patrolIndexes[patrol]] = i;
+            patrolIndexes[patrol]++;
+        } 
+	    this.array = array;
 	}
 
 	public String toString() {
