37f20dbe35c59d1245f02c972e97a24e94b24ace
zurc <afonsobpcruz@gmail.com>
2024-05-31 20:52:16 +0100

    resto do patrol_allocation

--- projeto/src/patrol_allocation/RandomHelper.java
+++ projeto/src/patrol_allocation/RandomHelper.java
@@ -1,17 +1,34 @@
 package patrol_allocation;
+
 import java.util.Random;
 
+/**
+ * A helper class for generating random numbers following specific distributions.
+ */
 class RandomHelper {
-	final Random random;
+    final Random random;
 
-	public RandomHelper(Random random) {
-		this.random = random;
-	}
+    /**
+     * Constructs a new RandomHelper with the specified random number generator.
+     *
+     * @param random The random number generator to use for generating random values.
+     */
+    public RandomHelper(Random random) {
+        this.random = random;
+    }
 
-	double getExp(double m) {
-		if (!Double.isFinite(m)) throw new IllegalArgumentException("Non-finite mean value for the exponential distribution");
-		double next = random.nextDouble();
-		double result = -m*Math.log(1.0-next);
-		return result;
-	}
+    /**
+     * Generates a random number following an exponential distribution with the specified mean.
+     *
+     * @param m The mean value for the exponential distribution.
+     * @return A random number following an exponential distribution with the specified mean.
+     * @throws IllegalArgumentException if the mean value is not finite.
+     */
+    double getExp(double m) {
+        if (!Double.isFinite(m)) throw new IllegalArgumentException("Non-finite mean value for the exponential distribution");
+        double next = random.nextDouble();
+        double result = -m * Math.log(1.0 - next);
+        return result;
+    }
 }
+
