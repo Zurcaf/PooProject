07a483ad8589f9291d38893642c2b70b5a3b5efe
zurc <afonsobpcruz@gmail.com>
2024-05-31 19:31:29 +0100

    erros de facto corrigidos

--- projeto/src/discrete_stochastic_simulation/PendingEventContainer.java
+++ projeto/src/discrete_stochastic_simulation/PendingEventContainer.java
@@ -1,19 +1,40 @@
 package discrete_stochastic_simulation;
 
+/**
+ * Interface representing a container for pending events in a discrete stochastic simulation.
+ *
+ * @param <A> The type of event action that this container handles, which must extend the {@link EventAction} interface.
+ */
 public interface PendingEventContainer<A extends EventAction> {
 
-	void removeEvent(TimedEvent<A> event);
+    /**
+     * Removes the specified event from the container.
+     *
+     * @param event The event to be removed.
+     */
+    void removeEvent(TimedEvent<A> event);
 
-	/**
-	 * 
-	 * @param time
-	 * @param execution
-	 */
-	void addEvent(TimedEvent<A> event);
+    /**
+     * Adds the specified event to the container.
+     *
+     * @param event The event to be added.
+     */
+    void addEvent(TimedEvent<A> event);
 
-	double currentEventTime();
+    /**
+     * Returns the current event time.
+     *
+     * @return The time of the current event.
+     */
+    double currentEventTime();
 
-	void run();
-	void stop();
+    /**
+     * Starts the execution of events in the container.
+     */
+    void run();
 
+    /**
+     * Stops the execution of events in the container.
+     */
+    void stop();
 }
--- projeto/src/discrete_stochastic_simulation/PriorityQueuePendingEventContainer.java
+++ projeto/src/discrete_stochastic_simulation/PriorityQueuePendingEventContainer.java
@@ -1,52 +1,77 @@
 package discrete_stochastic_simulation;
 
-import java.util.PriorityQueue;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.PriorityQueue;
 
+/**
+ * A container for pending events in a discrete stochastic simulation that uses a priority queue to manage events.
+ *
+ * @param <A> The type of event action that this container handles, which must extend the {@link EventAction} interface.
+ */
 public class PriorityQueuePendingEventContainer<A extends EventAction> implements PendingEventContainer<A>, Iterable<TimedEvent<A>> {
 
-	private PriorityQueue<TimedEvent<A>> pec = new PriorityQueue<TimedEvent<A>>();
-	private double currentEventTime = 0;
+    private PriorityQueue<TimedEvent<A>> pec = new PriorityQueue<TimedEvent<A>>();
+    private double currentEventTime = 0;
+    private boolean stopped = false;
 
-	private boolean stopped = false;
+    /**
+     * Removes the specified event from the container.
+     *
+     * @param oldTimedEvent The event to be removed.
+     */
+    public void removeEvent(TimedEvent<A> oldTimedEvent) {
+        pec.remove(oldTimedEvent);
+    }
 
+    /**
+     * Adds the specified event to the container.
+     *
+     * @param event The event to be added.
+     */
+    public void addEvent(TimedEvent<A> event) {
+        pec.add(event);
+    }
 
-	public void removeEvent(TimedEvent<A> oldTimedEvent) {
-		pec.remove(oldTimedEvent);
-	}	
+    /**
+     * Starts the execution of events in the container. Events are executed in order of their scheduled times.
+     * The execution stops when there are no more events or when the container is stopped.
+     * This method logs the current event time and the next 10 events left in the queue.
+     */
+    public void run() {
+        while (!stopped) {
+            TimedEvent<A> event = pec.poll();
+            if (event == null) break;
+            patrol_allocation.Debug.log("\n[" + event.time + "]");
+            currentEventTime = event.time;
+            event.action.execute();
+            patrol_allocation.Debug.log("Next 10 events left in queue: " + Arrays.deepToString(pec.stream().limit(10).map(ev -> ev.time + " " + ev.action.getClass().getName()).toArray()));
+        }
+        currentEventTime = -1;
+    }
 
-	/**
-	 * 
-	 * @param time
-	 * @param execution
-	 */
-	public void addEvent(TimedEvent<A> event) {
-		pec.add(event);
-	}
+    /**
+     * Stops the execution of events in the container.
+     */
+    public void stop() {
+        stopped = true;
+    }
 
-	public void run() {
-		while (!stopped) {
-			TimedEvent<A> event = pec.poll();
-			if (event == null) break;
-			patrol_allocation.Debug.log("\n[" + event.time + "]");
-			currentEventTime = event.time;
-			event.action.execute();
-			patrol_allocation.Debug.log("Next 10 events left in queue: " + Arrays.deepToString(pec.stream().limit(10).map(ev -> ev.time + " " + ev.action.getClass().getName()).toArray()));
-		}
-		currentEventTime = -1;
-	}
+    /**
+     * Returns the current event time.
+     *
+     * @return The time of the current event.
+     */
+    public double currentEventTime() {
+        return currentEventTime;
+    }
 
-	public void stop() {
-		stopped = true;
-	}
-
-	public double currentEventTime() {
-		return currentEventTime;
-	}
-
-	public Iterator<TimedEvent<A>> iterator() {
-		return pec.iterator();
-	}
-
+    /**
+     * Returns an iterator over the pending events in the container.
+     *
+     * @return An iterator over the pending events.
+     */
+    public Iterator<TimedEvent<A>> iterator() {
+        return pec.iterator();
+    }
 }
