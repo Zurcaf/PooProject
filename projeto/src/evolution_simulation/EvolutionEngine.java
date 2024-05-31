20ec4db268ed94c19dca9c639ed4cb84a3880b12
zurc <afonsobpcruz@gmail.com>
2024-05-31 20:04:50 +0100

    evolution_simulation javadoced

--- projeto/src/evolution_simulation/EvolutionEngine.java
+++ projeto/src/evolution_simulation/EvolutionEngine.java
@@ -2,29 +2,50 @@ package evolution_simulation;
 
 import java.util.List;
 
+/**
+ * Interface representing an evolution engine that manages a population of individuals,
+ * enforcing population limits and selecting the best individuals based on specific criteria.
+ *
+ * @param <I> The type of individuals managed by this evolution engine, which must implement the {@link Individual} interface.
+ */
 public interface EvolutionEngine<I extends Individual<I>> {
 
-	/**
-	 * 
-	 * @param individual the individual to add
-	 * @return whether an epidemic occurred because of this operation
-	 */
-	boolean addIndividual(I individual);
+    /**
+     * Adds an individual to the population. If the population exceeds the maximum limit,
+     * an epidemic is triggered to reduce the population.
+     *
+     * @param individual The individual to be added to the population.
+     * @return true if an epidemic was triggered, false otherwise.
+     */
+    boolean addIndividual(I individual);
 
-	/**
-	 * 
-	 * @param individual
-	 */
-	void removeIndividual(I individual);
+    /**
+     * Removes an individual from the population.
+     *
+     * @param individual The individual to be removed from the population.
+     */
+    void removeIndividual(I individual);
 
-	/**
-	 * 
-	 * @param count
-	 */
-	List<I> bestUniqueIndividuals(int count);
+    /**
+     * Gets the specified number of best individuals with unique solutions, as determined by
+     * {@code Individual.isSolutionEqual(I other)}. The best individuals are those with the highest comfort levels.
+     *
+     * @param count The number of unique best individuals to retrieve.
+     * @return A list of the best unique individuals.
+     */
+    List<I> bestUniqueIndividuals(int count);
 
-	int epidemicCount();
+    /**
+     * Returns the number of epidemics that have occurred.
+     *
+     * @return The number of epidemics.
+     */
+    int epidemicCount();
 
-	int populationCount();
-
+    /**
+     * Returns the current population count.
+     *
+     * @return The number of individuals in the population.
+     */
+    int populationCount();
 }
--- projeto/src/evolution_simulation/Individual.java
+++ projeto/src/evolution_simulation/Individual.java
@@ -1,11 +1,35 @@
 package evolution_simulation;
 
+/**
+ * Interface representing an individual in the evolution simulation. 
+ * Each individual must provide methods to evaluate its comfort, 
+ * determine if its solution is equal to another individual's solution, 
+ * and respond to an epidemic death.
+ *
+ * @param <I> The type of individual, which must implement the {@link Individual} interface.
+ */
 public interface Individual<I extends Individual<I>> {
 
-	double comfort();
+    /**
+     * Returns the comfort level of this individual. 
+     * The comfort level is a measure of the individual's suitability or fitness.
+     *
+     * @return The comfort level of this individual.
+     */
+    double comfort();
 
-	boolean isSolutionEqual(I other);
+    /**
+     * Determines if the solution of this individual is equal to the solution of another individual.
+     * This method is used to check if two individuals have the same solution.
+     *
+     * @param other The other individual to compare with.
+     * @return true if the solutions are equal, false otherwise.
+     */
+    boolean isSolutionEqual(I other);
 
-	void onEpidemicDeath();
-
+    /**
+     * Responds to an epidemic death. This method is called when an individual 
+     * is removed from the population due to an epidemic.
+     */
+    void onEpidemicDeath();
 }
