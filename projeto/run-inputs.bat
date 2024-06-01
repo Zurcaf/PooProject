ff11c01986979eca21e280e02ebc7af7020b3629
Duarte <duarte.pardal@tecnico.ulisboa.pt>
2024-06-01 14:45:05 +0100

    Alterados exemplos de input

---
+++ projeto/run-inputs.bat
@@ -0,0 +1,12 @@
+java -jar project.jar -f SIM/1x1_input.txt > SIM/1x1_output.txt
+java -jar project.jar -f SIM/1x100_input.txt > SIM/1x100_output.txt
+java -jar project.jar -f SIM/10k-population_input.txt > SIM/10k-population_output.txt
+java -jar project.jar -f SIM/50x50_input.txt > SIM/50x50_output.txt
+java -jar project.jar -f SIM/100x1_input.txt > SIM/100x1_output.txt
+java -jar project.jar -f SIM/100x100_input.txt > SIM/100x100_output.txt
+java -jar project.jar -f SIM/250x250_input.txt > SIM/250x250_output.txt
+java -jar project.jar -f SIM/500x500_input.txt > SIM/500x500_output.txt
+java -jar project.jar -f SIM/example_input.txt > SIM/example_output.txt
+java -jar project.jar -f SIM/many-epidemics_input.txt > SIM/many-epidemics_output.txt
+java -jar project.jar -f SIM/null-entries-on-some-columns_input.txt > SIM/null-entries-on-some-columns_output.txt
+java -jar project.jar -f SIM/short-lifetime_input.txt > SIM/short-lifetime_output.txt
